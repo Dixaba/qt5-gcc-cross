731b3e1c5558516252ecb069ad19fc9299ef6d58
Dixaba <dixaba@yandex.ru>
2020-01-24 02:18:05 +0300

    Cleaning

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 REPONAME=dixaba/qt5-gcc-cross
 
-for NAME in latestC-big latestC latestC-x64 latestC-both
+for NAME in latest-big latest latest-x64 latest-both
 do
-  FILENAME=$(sed -e 's/\-/\./g; s/latestC\.\?//; s/$/\.Dockerfile/; s/^\.//' <<< $NAME)
+  FILENAME=$(sed -e 's/\-/\./g; s/latest\.\?//; s/$/\.Dockerfile/; s/^\.//' <<< $NAME)
   docker build -f $FILENAME -t $REPONAME:$NAME .
   VERSION=$(grep -oP 'Using Qt version \K[0-9.]+' <<< $(docker run --rm $REPONAME:$NAME qmake --version))
   docker image tag $REPONAME:$NAME $REPONAME:$(sed "s/latest/$VERSION/" <<< $NAME)
